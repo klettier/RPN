36fad11c8db025ad5581a6accb2d73819bebb314
klettier <lettier.kevin@gmail.com>
2018-09-12 00:19:41 +0100

    Basic solution

---
+++ UnitTest1.fs
@@ -0,0 +1,61 @@
+namespace Tests
+
+open NUnit.Framework
+
+module Calculator =
+    let calcul opt state = 
+        match state with
+        | n1 :: n2 :: tail -> 
+            let result = opt n2 n1
+            printfn "n1 %i, n2 %i, result %i" n1 n2 result
+            result :: tail
+        | _ -> invalidOp "calcul"
+
+    let (|Number|_|) n = 
+        match System.Int32.TryParse n with
+        | true, v -> Some v
+        | _ -> None
+
+    let private agg (state:int list) (element:string) =
+        match element with
+        | Number(n) -> n :: state
+        | "+" -> calcul (+) state
+        | "-" -> calcul (-) state
+        | "/" -> calcul (/) state
+        | "*" -> calcul (*) state
+        | s -> invalidOp <| sprintf "Agg: %s" s
+    let calculate (st : string) = 
+        st.Split([|' '|])
+        |> Seq.fold agg []
+        |> Seq.head
+
+type CalculatorShould () =
+    [<Test>]
+    member __.``Return 2 for expression '1 1 +'`` () =
+        "1 1 +"
+        |> Calculator.calculate
+        |> fun v -> Assert.AreEqual(2, v)
+
+    [<Test>]
+    member __.``Return 3 for expression '1 1 1 + +'`` () =
+        "1 1 1 + +"
+        |> Calculator.calculate
+        |> fun v -> Assert.AreEqual(3, v)
+
+    [<Test>]
+    member __.``Return 1 for expression '1 1 1 - +'`` () =
+        "1 1 1 - +"
+        |> Calculator.calculate
+        |> fun v -> Assert.AreEqual(1, v)
+
+    [<Test>]
+    member __.``Return 18 for expression '2 3 11 + 5 - *'`` () =
+        "2 3 11 + 5 - *"
+        |> Calculator.calculate
+        |> fun v -> Assert.AreEqual(18, v)
+
+    [<Test>]
+    member __.``Return 5 for expression '15 7 1 1 + - / 3 * 2 1 1 + + -'`` () =
+        "15 7 1 1 + - / 3 * 2 1 1 + + -"
+        |> Calculator.calculate
+        |> fun v -> Assert.AreEqual(5, v)
